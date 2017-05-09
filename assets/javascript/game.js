73c3186b6726cff18345241729465dfcc3233ce3
Jon Berry <jonpber@gmail.com>
2017-05-09 08:05:43 -0700

    game pretty much working now with counter stats and everything.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,15 @@
+var heroHP = 0;
+var heroBaseAttack = 0;
+var heroAttackPower = 0;
+var foeHP = 0;
+var foeAttackPower = 0;
+
+
 $(function(){
-	$(".characterChoice").children().on("click", function(){
+	$(".characterChoice").on("click", "div", function(){
+		heroHP = $(this).find(".HP").text();
+		heroAttackPower = parseInt($(this).find(".attack").text());
+		heroBaseAttack = heroAttackPower;
 		$(".YourChar").append(this);
 		// $(".EnemiesToAttack").append(".characterChoice").children();
 		var children = $(".characterChoice").children();
@@ -10,9 +20,39 @@ $(function(){
 });
 
 $(function(){
-	$(".EnemiesToAttack").children().on("click", function(){
-		$(".Defender").append(this);
-	});
+	$(".EnemiesToAttack").on("click", "div", function(){
+		if ($(".Defender div").length === 0){
+			foeHP = $(this).find(".HP").text();
+			foeAttackPower = $(this).find(".counter").text();
+			$(".Defender").append(this);
+			$(".attackButton").show();
+		}
+		});
 });
+
+$(function(){
+	$(".attackButton").on("click", function(){
+		// console.log(heroAttackPower, foeAttackPower);
+		foeHP -= heroAttackPower;
+		heroAttackPower += heroBaseAttack;
+
+		if (foeHP > 0){
+			$(".Defender div").find(".HP").text(foeHP);
+			heroHP -= foeAttackPower;
+			$(".YourChar div").find(".HP").text(heroHP);
+
+
+		} else {
+			$(".Defender").empty();
+		}
+
+		});
+});
+
+// $(function(){
+// 	$(".Defender").on("click", "div", function(){
+// 		$(".Defender").empty();
+// 		});
+// });
 
 
